466837e9e968f5645a385cddb7a8892c55152b0c
Sarthak Swapnil Dubey <sarthaksd007@gmail.com>
2022-11-01 14:22:32 +0530

    Added Country selection and interactive plots

--- app.R
+++ app.R
@@ -5,48 +5,78 @@ source("script.R")
 
 ui <- dashboardPage(
   dashboardHeader(title = "2022 Covid stats"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Cases India", tabName = "tab1", icon = icon("globe")),
-      menuItem("Cases World", tabName = "tab2", icon = icon("virus-covid")),
-      menuItem("Vaccination India", tabName = "tab3", icon = icon("syringe")),
-      menuItem("vaccination world",tabName = "tab4",icon = icon("syringe"))
+  
+  dashboardSidebar(sidebarMenu(
+    menuItem(
+      "Cases By Country",
+      tabName = "tab1",
+      icon = icon("globe")
+    ),
+    menuItem(
+      "Cases World",
+      tabName = "tab2",
+      icon = icon("virus-covid")
+    ),
+    menuItem(
+      "Vaccinations By country",
+      tabName = "tab3",
+      icon = icon("syringe")
+    ),
+    menuItem(
+      "vaccination world",
+      tabName = "tab4",
+      icon = icon("syringe")
     )
-  ),
-  dashboardBody(
-    tabItems(
-      tabItem(tabName = "tab1",
-              fluidRow(
-                box(plotOutput("plot1",height = 700,width = 900))
-              )
-      ),
-      tabItem(tabName = "tab2",
-              fluidRow(
-                box(plotOutput("plot2",height = 700,width = 900))
-              )
-      ),
-      tabItem(tabName = "tab3",
-              fluidRow(
-                box(plotOutput("plot3",height = 700,width = 900))
-              )),
-      tabItem(tabName = "tab4",
-              fluidRow(
-                  box(plotOutput("plot4",height = 700,width = 900))
-      )
-      )
-      )
-    
-  )
+  )),
+  
+  dashboardBody(tabItems(
+    tabItem(tabName = "tab1",
+            sidebarLayout(
+              sidebarPanel(
+                selectInput(
+                  inputId = "countrychoose",
+                  label = "country",
+                  choices = unique(covid$location)
+                )
+              ),
+              mainPanel(plotlyOutput(
+                "plot1", height = 600, width = 800
+              ))
+            )),
+    tabItem(tabName = "tab2",
+            box(
+              plotOutput("plot2", height = 600, width = 800)
+            ))
+    ,
+    tabItem(tabName = "tab3",
+            sidebarLayout(
+              sidebarPanel(
+                selectInput(
+                  inputId = "vac_country",
+                  label = "country",
+                  choices = unique(covid$location)
+                )
+              ),
+              mainPanel(plotlyOutput(
+                "plot3", height = 600, width = 800
+              ))
+            )),
+    tabItem(tabName = "tab4",
+            fluidRow(box(
+              plotOutput("plot4", height = 700, width = 900)
+            )))
+  ))
 )
 
+
 server <- function(input, output, session) {
-  shiny.auto <- function(interval = 18*60*60){
+  shiny.auto <- function(interval = 18 * 60 * 60) {
     source("script.R")
     later::later(shiny.auto, interval)
   }
-  output$plot1 <- renderPlot(casevdeath_plot_IN)
+  output$plot1 <-renderPlotly(ggplotly(casevdeath_plot_country(countryfn(input$countrychoose))))
   output$plot2 <- renderPlot(casevdeath_plot_W)
-  output$plot3 <- renderPlot(vaccination_plot)
+  output$plot3 <- renderPlotly(ggplotly(vaccination_plot(countryfn(input$vac_country))))
   output$plot4 <- renderPlot(vaccination_plot_W)
   
 }
--- script.R
+++ script.R
@@ -1,63 +1,90 @@
 library(dplyr)
 library(ggplot2)
 library(lubridate)
+library(plotly)
 
-covid <- read.csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-india <- covid %>% filter(location=='India')%>%  
-  select(location,date,new_cases,new_deaths,new_vaccinations_smoothed)%>%  
-  mutate(date=as_date(date),year=year(date),month=month(date,label = TRUE)) %>%
-  filter(year=='2022') %>% 
-  arrange(month,new_cases,new_deaths)
+covid <- read.csv("owid-covid-data.csv")
+#covid <- read.csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+countryfn <- function(x) {
+  covid %>% filter(location == x) %>%
+    select(location,
+           date,
+           new_cases,
+           new_deaths,
+           new_vaccinations_smoothed) %>%
+    mutate(
+      date = as_date(date),
+      year = year(date),
+      month = month(date, label = TRUE)
+    ) %>%
+    filter(year == '2022') %>%
+    arrange(month, new_cases, new_deaths)
+}
+casevdeath_plot_country <-
+  function(countryfn) {
+    ggplot(countryfn, aes(x = date)) +
+      geom_line(aes(y = new_cases, color = 'New Cases')) +
+      labs(y = 'New Cases', x = 'Months') +
+      geom_line(aes(y = new_deaths, color = 'Deaths')) +
+      scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+      scale_y_continuous(sec.axis = sec_axis( ~ . * 1, name = "Deaths")) +
+      theme_light() +
+      theme(legend.position = "bottom",
+            plot.title = element_text(hjust = 0.5),
+            axis.text.y.left = element_blank(),
+            axis.text.y.right = element_blank()) +
+      scale_color_manual(
+        name = "Covid Cases",
+        values =  c("steelblue", "darkred"),
+        breaks = c("New Cases", "Deaths")
+      ) +
+      ggtitle(paste("Covid-19 New Cases vs Deaths in ", unique(countryfn$location)))
+  }
 
-casevdeath_plot_IN <- ggplot(india,aes(x=date)) +
-  geom_line(aes(y = new_cases/2,color='New Cases')) +
-  labs(y='New Cases',x='Months') +
-  geom_line(aes(y = new_deaths/0.025,color='Deaths')) +
-  scale_x_date(date_breaks = "1 month",date_labels = "%b") +
-  scale_y_continuous(sec.axis = sec_axis(~.*0.025, name = "Deaths")) +
-  theme_light() +
-  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(name="Covid Cases",
-                     values =  c("steelblue","darkred"),breaks = c("New Cases", "Deaths")) +
-  ggtitle("Covid-19 New Cases vs Deaths in India")
-
-vaccination_plot <- ggplot(india,aes(x=date)) + 
-  geom_line(aes(y = new_vaccinations_smoothed,color='New vaccinations')) +
-  labs(y='New Vaccinations',x='Months') +
-  scale_x_date(date_breaks = "1 month",date_labels = "%b") +
-  theme_light() +
-  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(name="Vaccinations",values =  c("steelblue"),breaks = c("New vaccinations")) +
-  ggtitle("Vaccinations in India")
-
-rm(india)
-
-World <- covid %>% filter(location=='World')%>%  
-  select(location,date,new_cases,new_deaths,new_vaccinations_smoothed)%>%  
-  mutate(date=as_date(date),year=year(date),month=month(date,label = TRUE)) %>%
-  filter(year=='2022') %>% 
-  arrange(month,new_cases,new_deaths)
-
-casevdeath_plot_W <- ggplot(World,aes(x=date)) +
-  geom_line(aes(y = new_cases/2,color='New Cases')) +
-  labs(y='New Cases',x='Months') +
-  geom_line(aes(y = new_deaths/0.025,color='Deaths')) +
-  scale_x_date(date_breaks = "1 month",date_labels = "%b") +
-  scale_y_continuous(sec.axis = sec_axis(~.*0.025, name = "Deaths")) +
-  theme_light() +
-  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(name="Covid Cases",
-                     values =  c("steelblue","darkred"),breaks = c("New Cases", "Deaths")) +
-  ggtitle("Covid-19 New Cases vs Deaths in World")
-
-vaccination_plot_W <- ggplot(World,aes(x=date)) + 
-  geom_line(aes(y = new_vaccinations_smoothed,color='New vaccinations')) +
-  labs(y='New Cases',x='Months') +
-  scale_x_date(date_breaks = "1 month",date_labels = "%b") +
-  theme_light() +
-  theme(legend.position="bottom",plot.title = element_text(hjust = 0.5)) +
-  scale_color_manual(name="Vaccinations",values =  c("steelblue"),breaks = c("New vaccinations")) +
-  ggtitle("Vacinations around the world")
-
-rm(World)
-rm(covid)
+vaccination_plot <- function(x) {
+    ggplot(x, aes(x = date)) +
+    geom_line(aes(y = new_vaccinations_smoothed, color = 'New vaccinations')) +
+    labs(y = 'New Vaccinations', x = 'Months') +
+    scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+    theme_light() +
+    theme(legend.position = "bottom",
+          plot.title = element_text(hjust = 0.5)) +
+    scale_color_manual(
+      name = "Vaccinations",
+      values =  c("steelblue"),
+      breaks = c("New vaccinations")
+    ) +
+    ggtitle(paste("vaccinations in ",unique(x$location)))
+}
+# #rm(countryfn)
+#
+# World <- covid %>% filter(location=='World')%>%
+#   select(location,date,new_cases,new_deaths,new_vaccinations_smoothed)%>%
+#   mutate(date=as_date(date),year=year(date),month=month(date,label = TRUE)) %>%
+#   filter(year=='2022') %>%
+#   arrange(month,new_cases,new_deaths)
+#
+# casevdeath_plot_W <- ggplot(World,aes(x=date)) +
+#   geom_line(aes(y = new_cases/2,color='New Cases')) +
+#   labs(y='New Cases',x='Months') +
+#   geom_line(aes(y = new_deaths/0.025,color='Deaths')) +
+#   scale_x_date(date_breaks = "1 month",date_labels = "%b") +
+#   scale_y_continuous(sec.axis = sec_axis(~.*0.025, name = "Deaths")) +
+#   theme_light() +
+#   theme(legend.position="bottom",plot.title = element_text(hjust = 0.5)) +
+#   scale_color_manual(name="Covid Cases",
+#                      values =  c("steelblue","darkred"),breaks = c("New Cases", "Deaths")) +
+#   ggtitle("Covid-19 New Cases vs Deaths in World")
+#
+# vaccination_plot_W <- ggplot(World,aes(x=date)) +
+#   geom_line(aes(y = new_vaccinations_smoothed,color='New vaccinations')) +
+#   labs(y='New Cases',x='Months') +
+#   scale_x_date(date_breaks = "1 month",date_labels = "%b") +
+#   theme_light() +
+#   theme(legend.position="bottom",plot.title = element_text(hjust = 0.5)) +
+#   scale_color_manual(name="Vaccinations",values =  c("steelblue"),breaks = c("New vaccinations")) +
+#   ggtitle("Vacinations around the world")
+#
+# #rm(World)
+# #rm(covid)
+# casevdeath_plot_IN
